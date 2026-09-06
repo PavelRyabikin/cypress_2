256bbdd1aade310412e2c9fa2998ee72194276f3
Pavel Ryabikin <paulryabikin@gmail.com>
2026-09-06 11:06:09 +0300

    Fix admin login test

--- cypress/e2e/admin-login.cy.js
+++ cypress/e2e/admin-login.cy.js
@@ -1,17 +1,19 @@
 describe("admin login", () => {
   it("should login to admin", () => {
     cy.fixture("login").then((data) => {
-      cy.visit("/admin/");
+      cy.fixture("selectors").then((selectors) => {
+        cy.visit("/admin/");
 
-      cy.login(data.happy.email, data.happy.password);
-      cy.contains("Управление залами").should("be.visible");
+        cy.login(data.happy.email, data.happy.password, selectors.admin);
+        cy.contains("Управление залами").should("be.visible");
 
-      cy.clearCookies();
+        cy.clearCookies();
 
-      cy.visit("/admin/");
+        cy.visit("/admin/");
 
-      cy.login(data.sad.email, data.sad.password);
-      cy.contains("Ошибка авторизации!").should("be.visible");
+        cy.login(data.sad.email, data.sad.password, selectors.admin);
+        cy.contains("Ошибка авторизации!").should("be.visible");
+      });
     });
   });
 });
